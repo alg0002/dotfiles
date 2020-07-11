00c034f1e065ab00e6a474363066b67f5fd43676
alg <alg.info@gmail.com>
2020-07-11 19:20:07 +0900

    Change gitbash prompt

--- .config/git/git-prompt.sh
+++ .config/git/git-prompt.sh
@@ -1,11 +1,12 @@
+# For Windows gitbash
+
 PS1='\[\033]0;$TITLEPREFIX:$PWD\007\]' # set window title
 PS1="$PS1"'\n'                 # new line
-PS1="$PS1"'\[\033[32m\]'       # change to green
-PS1="$PS1"'\u@\h '             # user@host<space>
-PS1="$PS1"'\[\033[35m\]'       # change to purple
-PS1="$PS1"'$MSYSTEM '          # show MSYSTEM
-PS1="$PS1"'\[\033[33m\]'       # change to brownish yellow
-PS1="$PS1"'\w'                 # current working directory
+PS1="$PS1"'\[\033[4;32m\]'     # change style to underline;green
+PS1="$PS1"'\D{%m/%d} '         # date (mm/dd)
+PS1="$PS1"'\t'                 # time (hh:mi:ss)
+PS1="$PS1"'\[\033[0;33;49m\]'  # change style to default;yellow;default
+PS1="$PS1"' \w'                # current working directory
 if test -z "$WINELOADERNOEXEC"
 then
 	GIT_EXEC_PATH="$(git --exec-path 2>/dev/null)"
@@ -20,6 +21,6 @@ then
 		PS1="$PS1"'`__git_ps1`'   # bash function
 	fi
 fi
-PS1="$PS1"'\[\033[0m\]'        # change color
+PS1="$PS1"'\[\033[0m\]'        # change style to default
 PS1="$PS1"'\n'                 # new line
 PS1="$PS1"'$ '                 # prompt: always $
