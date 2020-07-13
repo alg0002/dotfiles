1f8c04e7d1f922faf5447deb57cd621997e5cf8b
alg <alg.info@gmail.com>
2020-07-13 13:03:11 +0900

    gitbashをPowerline風に変更

--- .config/git/git-prompt.sh
+++ .config/git/git-prompt.sh
@@ -2,11 +2,15 @@
 
 PS1='\[\033]0;$TITLEPREFIX:$PWD\007\]' # set window title
 PS1="$PS1"'\n'                 # new line
-PS1="$PS1"'\[\033[4;32m\]'     # change style to underline;green
-PS1="$PS1"'\D{%m/%d} '         # date (mm/dd)
-PS1="$PS1"'\t'                 # time (hh:mi:ss)
-PS1="$PS1"'\[\033[0;33;49m\]'  # change style to default;yellow;default
-PS1="$PS1"' \w'                # current working directory
+PS1="$PS1"'\[\033[0;30;42m\]'  # change style to normal;black;green
+PS1="$PS1"' \D{%m/%d} '        # date (mm/dd)
+PS1="$PS1"'\t '                # time (hh:mi:ss)
+PS1="$PS1"'\[\033[0;32;102m\]' # change style to normal;green;lightgreen
+PS1="$PS1"''                  # sankaku
+PS1="$PS1"'\[\033[0;30;102m\]' # change style to normal;black;lightgreen
+PS1="$PS1"' \w '               # current working directory
+PS1="$PS1"'\[\033[0;92;44m\]'  # change style to normal;lightgreen;blue
+PS1="$PS1"''                  # sankaku
 if test -z "$WINELOADERNOEXEC"
 then
 	GIT_EXEC_PATH="$(git --exec-path 2>/dev/null)"
@@ -17,10 +21,15 @@ then
 	then
 		. "$COMPLETION_PATH/git-completion.bash"
 		. "$COMPLETION_PATH/git-prompt.sh"
-		PS1="$PS1"'\[\033[36m\]'  # change color to cyan
-		PS1="$PS1"'`__git_ps1`'   # bash function
+		PS1="$PS1"'\[\033[0;37;44m\]'  # change style to normal;gray;blue
+		PS1="$PS1"'`__git_ps1 " %s "`' # bash function
+		PS1="$PS1"'\[\033[0;34;49m\]'  # change style to normal;blue;default
+		PS1="$PS1"''                  # sankaku
 	fi
 fi
-PS1="$PS1"'\[\033[0m\]'        # change style to default
 PS1="$PS1"'\n'                 # new line
-PS1="$PS1"'$ '                 # prompt: always $
+PS1="$PS1"'\[\033[0;30;47m\]'  # change style to normal;black;gray
+PS1="$PS1"'$'                  # prompt: always $
+PS1="$PS1"'\[\033[0;37;49m\]'  # change style to norma;gray;default
+PS1="$PS1"' '                 # sankaku
+PS1="$PS1"'\[\033[0m\]'        # change style to default
